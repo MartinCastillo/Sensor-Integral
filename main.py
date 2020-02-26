046e4792e0fe651f4c745373bb34801e224df730
Martín <43865973+MartinCastillo@users.noreply.github.com>
2020-02-26 12:56:25 -0300

    coments

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import time
-from math import sqrt
 #Function to get time in milliseconds like millis in Arduino
 millis = lambda : time.time_ns() // 1000000
 
-#Function semi-stolen from stackoverflow to generate a pseudo random signal
+#Function semi-stolen from stackoverflow to generate a pseudo random curve
 #https://stackoverflow.com/questions/29050164/produce-random-wavefunction
 X = np.linspace(1, 1000,1000)
 def f(x,N=10):
+    #Return an array of values that simulates the curve/signal
     y = 0;result = []
     for _ in x:
         result.append(y)
@@ -18,24 +18,21 @@ def f(x,N=10):
     x = np.array(result)
     return np.convolve(x, np.ones((N,))/N)[(N-1):]
 
-'''def f(x):
-    return [sqrt(x) for x in range(0,100)]'''
-
 signal = f(X)
-#Simulate a random signal and aply the double integration
+#Simulate a random curve and aply the double integration
 if(__name__=='__main__'):
-    pila = [] #The storage for the recived signal values
-    sums = [] #The acumulative values of the integral thorugh time
+    pila = [] #The storage for the recived curve values
+    sums = [] #The acumulative values of the integral over time
     sum = 0
-    times = []#Record time for each signal input
+    times = []#Record time for each curve´s input
 
-    ############################Signal through time############################
+    ############################ Signal/Curve over time ############################
     plt.ion()
     for (s_ix,s) in enumerate(signal):
         pila.append([millis(),s])
-        if(len(pila)>=4): #Get the last 4 values of the signal
+        if(len(pila)>=4): #Get the last 4 values of the curve´s recived data
             pila = pila[-4:]
-            #Add the area of the current signal section to the rest of the area(the last)
+            #Add the area of the current curve section to the rest of the area(the last)
             sum += integrate_points(integrate_points(pila))[-1][-1]
             sums.append(sum)
             times.append(millis())
